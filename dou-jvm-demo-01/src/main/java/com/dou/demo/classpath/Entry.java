54323ecc686f00340e4ddafb0d4e12e02752f36b
fanhengxue_nj <fhx0102@163.com>
2025-02-18 20:30:14 +0800

    test

---
+++ dou-jvm-demo-01/src/main/java/com/dou/demo/classpath/Entry.java
@@ -0,0 +1,11 @@
+package com.dou.demo.classpath;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: fanhengxue
+ * @Date: 2025/02/18/20:16
+ * @Description:类路径接口
+ */
+public interface Entry {
+}
