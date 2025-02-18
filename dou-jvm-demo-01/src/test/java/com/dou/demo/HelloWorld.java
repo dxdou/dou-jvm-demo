1a2c6c54ed913d450717fad219110ccb354c1588
fanhengxue_nj <408531813@qq.com>
2025-02-18 20:07:56 +0800

    first commit

---
+++ dou-jvm-demo-01/src/test/java/com/dou/demo/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.dou.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: fanhengxue
+ * @Date: 2025/02/18/20:03
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0; i < 10; i++) {
+             num += i;
+        }
+        System.out.println("Hello World!  " + num);
+    }
+}
